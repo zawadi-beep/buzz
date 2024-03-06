fa02afbbbfaf0f19075ed4de6d710a7e54197c61
tess585 <tesswangui7@gmail.com>
2024-03-06 13:01:50 +0300

    first commit

--- buzzapp/urls.py
+++ buzzapp/urls.py
@@ -1,19 +1,5 @@
-"""
-URL configuration for Buzz project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+
 from django.contrib import admin
 from django.urls import path
 from buzzapp import views
@@ -22,13 +8,20 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name = 'index'),
     path('inner-page/', views.inner, name = 'inner'),
+
     path('portfolio/', views.Portfolio, name = 'portfolio'),
     path('register/', views.register, name = 'register'),
     path('login/', views.login, name = 'login'),
+
     path('details/', views.detail, name = 'detail'),
     path('user-details/', views.user, name = 'user'),
     path('adminhome/', views.adminhome, name = 'adminhome'),
+
     path('uploadimage/', views.upload_image, name='upload'),
     path('showimage/', views.show_image, name='image'),
     path('imagedelete/<int:id>', views.imagedelete),
+
+    path('pay/', views.pay, name='pay'),
+    path('stk/', views.stk, name='stk'),
+    path('token/', views.token, name='token'),
 ]
--- buzzapp/views.py
+++ buzzapp/views.py
@@ -1,6 +1,12 @@
+import json
 from typing import Tuple
 
+import requests
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from requests.auth import HTTPBasicAuth
+
+from buzzapp.credentials import MpesaAccessToken, LipanaMpesaPpassword
 from buzzapp.models import Member
 from buzzapp.models import Contact
 from buzzapp.models import Users,ImageModel
@@ -95,4 +101,44 @@ def imagedelete(request, id):
     image = ImageModel.objects.get(id=id)
     image.delete()
     return redirect('/showimage')
+def token(request):
+    consumer_key = 'qDNCMWmL0XQC6qL18v0v5nVSEW659LApnKi9YPB4lgGGjRk6'
+    consumer_secret = 'MF8ZwqM9WPuXlAMEHxu0bda63O3cwibFj8ztjw2FhkpGCAX59KBricdzLuxDDnRU'
+    api_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
+
+    r = requests.get(api_URL, auth=HTTPBasicAuth(
+        consumer_key, consumer_secret))
+    mpesa_access_token = json.loads(r.text)
+    validated_mpesa_access_token = mpesa_access_token["access_token"]
+
+    return render(request, 'token.html', {"token":validated_mpesa_access_token})
+
+def pay(request):
+   return render(request, 'pay.html')
+
+
+
+def stk(request):
+    if request.method =="POST":
+        phone = request.POST['phone']
+        amount = request.POST['amount']
+        access_token = MpesaAccessToken.validated_mpesa_access_token
+        api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+        headers = {"Authorization": "Bearer %s" % access_token}
+        request = {
+            "BusinessShortCode": LipanaMpesaPpassword.Business_short_code,
+            "Password": LipanaMpesaPpassword.decode_password,
+            "Timestamp": LipanaMpesaPpassword.lipa_time,
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount": amount,
+            "PartyA": phone,
+            "PartyB": LipanaMpesaPpassword.Business_short_code,
+            "PhoneNumber": phone,
+            "CallBackURL": "https://sandbox.safaricom.co.ke/mpesa/",
+            "AccountReference": "Apen Softwares",
+            "TransactionDesc": "Web Development Charges"
+        }
+        response = requests.post(api_url, json=request, headers=headers)
+        return HttpResponse(response)
+
 
